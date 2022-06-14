ad9c8e87ad70ccf20f104a91b3283a63851d9fcb
Anastasiya Scshekotina <lenavl@list.ru>
2022-06-14 13:54:41 +0700

    Showing of meals' list: var2 (using JSTL)

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -7,27 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.format.DateTimeFormatter;
 
 public class MealServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=utf-8");
-        PrintWriter pw = response.getWriter();
-        pw.println("<table border=\"1\" cellpadding=\"6\">");
-        pw.println("<tr><th>Date</th><th>Description</th><th>Calories</th></tr>");
-        MealsUtil.MEALSTO.forEach(meal -> {
-            String color = meal.isExcess() ? "red" : "green";
-            pw.println("<tr style=\"color:" + color + "\"><td>" + meal.getDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm")) + "</td>" +
-                    "<td>" + meal.getDescription() + "</td>" +
-                    "<td>" + meal.getCalories() + "</td></tr>");
-        });
-        pw.println("</table>");
-
-//        request.setAttribute("meals", meals);
-//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        request.setAttribute("meals", MealsUtil.MEALSTO);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -5,12 +5,33 @@
   Time: 10:31
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@taglib uri="https://example.com/functions" prefix="f" %>
+<%@ page contentType="text/html;charset=UTF-8"%>
 <html>
 <head>
+    <link rel="stylesheet" href="styles.css">
     <title>Title</title>
 </head>
 <body>
 <h3><a href="index.html">Home</a></h3>
+<table>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    <c:forEach var="meal" items="${meals}">
+        <c:set var="color" value="green"/>
+        <c:if test="${meal.excess}">
+            <c:set var="color" value="red"/>
+        </c:if>
+        <tr style="color:${color}">
+            <td>${f:formatLocalDateTime(meal.dateTime, "yyyy-MM-dd hh:mm")}</td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
